6b6cfd15e49a9d269ec4be1a59e5711bdc471964
Shakhawat Bijoy <piyash2003b@gmail.com>
2025-11-14 18:34:39 +0600

    JavaScript loops

---
+++ day-3/script.js
@@ -0,0 +1,30 @@
+// loops
+// for loop
+// while loop
+// do while loop
+
+// for loop
+
+for(let i = 1; i < 10; i++){
+    console.log();
+}
+
+
+// While loop
+
+let i = 1;
+while(i<32){
+    //code
+    console.log();
+    i++
+}
+
+
+// do while 
+
+let j = 12;
+do{
+    console.log(j);
+    j++
+}
+while(j<2)
